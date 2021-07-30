78ded73ac5a505f2c5a7bbaf8eca3522cb4c1b1f
arnavpathak <arnavpathak230@gmail.com>
2021-07-30 17:15:12 -0400

    Process thrust and arrow keys logs

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE-thrusting");
+        }
+        
+    }
+    void ProcessRotation()
+    {
+       if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed A-rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed D-rotating right");
+        }
+    }
+}
